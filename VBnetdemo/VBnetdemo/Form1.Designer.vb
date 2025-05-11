6e293342899f414fb14f5652b0387b418c040bd7
windyHZYY <59727695+windyHZYY@users.noreply.github.com>
2025-05-11 09:57:49 +0800

    修改csvTool追加写入数据的功能，加入Vb.net调用示例

---
+++ VBnetdemo/VBnetdemo/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInit = New System.Windows.Forms.Button()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnInit
+        '
+        Me.btnInit.Location = New System.Drawing.Point(39, 66)
+        Me.btnInit.Name = "btnInit"
+        Me.btnInit.Size = New System.Drawing.Size(196, 109)
+        Me.btnInit.TabIndex = 0
+        Me.btnInit.Text = "Init"
+        Me.btnInit.UseVisualStyleBackColor = True
+        '
+        'btnWrite
+        '
+        Me.btnWrite.Location = New System.Drawing.Point(327, 66)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(165, 82)
+        Me.btnWrite.TabIndex = 1
+        Me.btnWrite.Text = "Write"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(79, 269)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(100, 21)
+        Me.txtAddress.TabIndex = 2
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(214, 269)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(100, 21)
+        Me.txtAge.TabIndex = 2
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(327, 269)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 21)
+        Me.txtName.TabIndex = 2
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(456, 269)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(100, 21)
+        Me.txtPhone.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(466, 231)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "txtPhone"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(340, 231)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "txtName"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(228, 231)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "txtAge"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(86, 231)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "txtAddress"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(871, 532)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Controls.Add(Me.btnInit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnInit As Button
+    Friend WithEvents btnWrite As Button
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ VBnetdemo/VBnetdemo/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports FeiyaoAutoBaseLib
+
+Public Class Form1
+    Dim CSVRWTool_Object As New CSVRWTool()
+    Dim CSVFilePath As String = "D:\example.csv"
+    Dim exData As New ExData()
+    Private Sub btnInit_Click(sender As Object, e As EventArgs) Handles btnInit.Click
+        CSVRWTool_Object = New CSVRWTool(CSVFilePath)
+    End Sub
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        exData.Address = txtAddress.Text
+        exData.Age = CInt(txtAge.Text)
+        exData.Name = txtName.Text
+        exData.Phone = txtPhone.Text
+        CSVRWTool_Object.AppendWriteCsvRecord(exData)
+    End Sub
+End Class
+
+
+Public Class ExData
+    Public Property Name As String
+    Public Property Age As Integer
+    Public Property Address As String
+    Public Property Phone As String
+    Public Sub New()
+        Name = ""
+        Age = 0
+        Address = ""
+        Phone = ""
+    End Sub
+    Public Sub New(name As String, age As Integer, address As String, phone As String)
+        Me.Name = name
+        Me.Age = age
+        Me.Address = address
+        Me.Phone = phone
+    End Sub
+End Class
